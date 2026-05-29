066436cdf38dfa108b7f75b1fb6ca47349736c5a
scuynat <skynet@cyberdyne.hu>
2026-05-29 15:59:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+
+app = Flask(__name__)
+
+SECRET = "12346782343241236328723756"
+
+@app.route("/")
+def hello():
+    return "OK"
+
+@app.route("/lock")
+def lock():
+    if request.args.get("secret") != SECRET:
+        return "Forbidden", 403
+
+    return "Lock request received"from flask import Flask
 
 app = Flask(__name__)
 
