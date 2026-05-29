d185ea6906be6d815c2d6c8e70025794452af695
scuynat <skynet@cyberdyne.hu>
2026-05-29 18:50:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 SECRET = "12346782343241236328723756"
 
 @app.route("/")
-def hello():
+def root():
     return "OK"
 
 @app.route("/lock")
@@ -13,5 +14,16 @@ def lock():
     if request.args.get("secret") != SECRET:
         return "Forbidden", 403
 
-    return "Lock request received"
+    vm = VehicleManager(
+        region=1,                 # Europe
+        brand=2,                  # Hyundai
+        username=os.environ["HYUNDAI_USER"],
+        password=os.environ["HYUNDAI_PASS"],
+        pin=""
+    )
+
+    vm.check_and_refresh_token()
+    vm.update_all_vehicles_with_cached_state()
+
+    return str(vm.vehicles)
     
