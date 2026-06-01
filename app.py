07036eaace67fa588b572cc1cc7aa49d4c242012
scuynat <skynet@cyberdyne.hu>
2026-06-01 18:22:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,23 +24,8 @@ def lock():
     )
 
     vm.check_and_refresh_token()
-    vm.update_all_vehicles_with_cached_state()
+    return "<br>".join(dir(vm))
+    
 
-    try:
-        vehicle = next(iter(vm.vehicles.values()))
-        s = vehicle.data["vehicleStatus"]
-
-        return jsonify({
-            "locked": s["doorLock"],
-            "engine": s["engine"],
-            "frontLeft": s["doorOpen"]["frontLeft"],
-            "frontRight": s["doorOpen"]["frontRight"],
-            "backLeft": s["doorOpen"]["backLeft"],
-            "backRight": s["doorOpen"]["backRight"],
-            "trunkOpen": s["trunkOpen"]
-        })
-        
-    except Exception as e:
-        return str(e), 500
 
     
