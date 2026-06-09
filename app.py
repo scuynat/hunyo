7c4782d6373e6c8234df506fb000c6336835cc59
scuynat <skynet@cyberdyne.hu>
2026-06-09 11:07:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,36 @@ SECRET = os.environ["SECRET"]
 @app.route("/")
 def root():
     return "OK"
-
+    
 @app.route("/lock")
 def lock():
+
     if request.args.get("secret") != SECRET:
         return "Forbidden", 403
 
-    vm = VehicleManager(
-        region=1,                 # Europe
-        brand=2,                  # Hyundai
-        username=os.environ["HYUNDAI_USER"],
-        password=os.environ["HYUNDAI_PASS"],
-        pin=os.environ["HYUNDAI_PIN"]
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.environ["PUSHOVER_API_TOKEN"],
+            "user": os.environ["PUSHOVER_USER_KEY"],
+            "message": "TESTING"
+        }
     )
+    return "Test OK"
 
-    vm.check_and_refresh_token()
-    vm.force_refresh_all_vehicles_states()
-    vm.update_all_vehicles_with_cached_state()
-
-    try:       
+    
+    try: 
+        vm = VehicleManager(
+            region=1,                 # Europe
+            brand=2,                  # Hyundai
+            username=os.environ["HYUNDAI_USER"],
+            password=os.environ["HYUNDAI_PASS"],
+            pin=os.environ["HYUNDAI_PIN"]
+        )
+    
+        vm.check_and_refresh_token()
+        vm.force_refresh_all_vehicles_states()
+        vm.update_all_vehicles_with_cached_state()          
         vehicle = next(iter(vm.vehicles.values()))
         s = vehicle.data["vehicleStatus"]
 
